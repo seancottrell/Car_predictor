a777d817b4e43d8072459278445c2ad811bf1d60
Aharr380 <97995177+Aharr380@users.noreply.github.com>
2022-03-29 00:29:12 -0400

    updated the app.py file

--- demo/app.py
+++ demo/app.py
@@ -6,4 +6,27 @@ app = Flask(__name__)
 def homepage():
     return render_template("home.html")
 
+@app.route("/about")
+def projectwriteup():
+    return render_template("project_writeup.html")
+
+@app.route("/Alec_Harrod")
+def alec():
+    return render_template("resume_alec.html")
+
+@app.route("/Roshane_Bent")
+def roshane():
+    return render_template("resume_roshane.html")
+
+@app.route("/Isaac_Matsko")
+def isaac():
+    return render_template("resume_isaac.html")
+
+@app.route("/Sean_Cottrell")
+def sean():
+    return render_template("resume_sean.html")
+
+
+if __name__ == '__main__':
+    app.run(debud=True)
 #change homepage.html to the html file in your templates folder
